18c72279a60d173d6fae73d98869973df0a2fde5
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-03-17 18:42:42 +0100

    association template to link ,using spacename

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
+
+    # ex: /polls/5/results/
+    path('<int:question_id>/', views.detail, name='name_detail'),
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,26 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from django.shortcuts import render
 from .models import Question
+from django.http import Http404
+""" fonction pour rendre un template avec HttpResponse """
+"""def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))"""
 
-
+""" fonction pour rendre un template avec render """
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question n'exist pas")
+    return render(request, 'polls/detail.html', {'question': question.__repr__() })
