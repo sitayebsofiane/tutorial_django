6e54a6ae06720f955328b70952aac697bb5bda7e
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-03-17 20:49:29 +0100

    modify acces template

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,7 +9,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __repr__(self):
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,12 +3,9 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
-
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-
-    # ex: /polls/5/results/
-    path('<int:question_id>/', views.detail, name='name_detail'),
-
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
