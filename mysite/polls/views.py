91058987ae706080dbe6c8af78b5f4636975fb74
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-03-19 12:21:03 +0100

    add page home

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -25,6 +25,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -42,4 +43,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
